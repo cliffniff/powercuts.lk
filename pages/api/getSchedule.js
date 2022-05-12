61038e30a2077fd6e2f19ba474d9663340332402
Sakith B <pvsakith@gmail.com>
2022-05-12 11:43:47 +0530

    Removed SSG

---
+++ pages/api/getSchedule.js
@@ -0,0 +1,52 @@
+import DOMParser from "dom-parser";
+
+export default async function handler(req, res) {
+    try {
+        const pageRes = await fetch("https://cebcare.ceb.lk/Incognito/DemandMgmtSchedule");
+        const pageContent = await pageRes.text();
+
+        const domParser = new DOMParser();
+        const parsedDoc = domParser.parseFromString(pageContent, "text/html");
+
+        const reqToken = parsedDoc
+            .getElementsByName("__RequestVerificationToken")[0]
+            .getAttribute("value");
+
+        const reqCookie = pageRes.headers.get("set-cookie").split(";")[0];
+
+        const dateRef = new Date();
+        const startTime = `${dateRef.getFullYear()}-${dateRef.getMonth() + 1}-${dateRef.getDate()}`;
+        dateRef.setDate(dateRef.getDate() + 1);
+        const endTime = `${dateRef.getFullYear()}-${dateRef.getMonth() + 1}-${dateRef.getDate()}`;
+
+        const scheduleRes = await fetch("https://cebcare.ceb.lk/Incognito/GetLoadSheddingEvents", {
+            headers: {
+                requestverificationtoken: reqToken,
+                cookie: reqCookie,
+            },
+            cache: "no-cache",
+            body: `StartTime=${startTime}&EndTime=${endTime}`,
+            method: "POST",
+        });
+
+        const scheduleData = await scheduleRes.json();
+        const schedule = {};
+
+        scheduleData.forEach((item) => {
+            const gid = item.loadShedGroupId;
+            const stime = item.startTime.replace("T", " ");
+            const etime = item.endTime.replace("T", " ");
+
+            if (schedule[gid] === undefined) {
+                schedule[gid] = [{ startTime: stime, endTime: etime }];
+            } else {
+                schedule[gid].push({ startTime: stime, endTime: etime });
+            }
+        });
+
+        res.json(schedule);
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({});
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,73 +1,38 @@
 import { useState, useEffect } from "react";
-import Head from "next/head";
+import { MdError } from "react-icons/md";
+import { CgSpinner } from "react-icons/cg";
 
-import fs from "fs/promises";
-import DOMParser from "dom-parser";
+import Head from "next/head";
 
 import Group from "../components/Group";
 import Navbar from "../components/Navbar";
 
 import styles from "../styles/Home.module.css";
 
-export const getServerSideProps = async () => {
-    try {
-        const pageRes = await fetch("https://cebcare.ceb.lk/Incognito/DemandMgmtSchedule");
-        const pageContent = await pageRes.text();
+export default function Home() {
+    const [highlighted, setHighlighted] = useState();
+    const [schedule, setSchedule] = useState();
+    const [geoAreas, setGeoAreas] = useState();
 
-        const domParser = new DOMParser();
-        const parsedDoc = domParser.parseFromString(pageContent, "text/html");
+    const [error, setError] = useState(false);
 
-        const reqToken = parsedDoc
-            .getElementsByName("__RequestVerificationToken")[0]
-            .getAttribute("value");
+    useEffect(() => {
+        (async () => {
+            try {
+                const scheduleRes = await fetch("/api/getSchedule");
+                const schedule = await scheduleRes.json();
 
-        const reqCookie = pageRes.headers.get("set-cookie").split(";")[0];
+                const geoRes = await fetch("/geoAreas.json");
+                const geoAreas = await geoRes.json();
 
-        const dateRef = new Date();
-        const startTime = `${dateRef.getFullYear()}-${dateRef.getMonth() + 1}-${dateRef.getDate()}`;
-        dateRef.setDate(dateRef.getDate() + 1);
-        const endTime = `${dateRef.getFullYear()}-${dateRef.getMonth() + 1}-${dateRef.getDate()}`;
-
-        const scheduleRes = await fetch("https://cebcare.ceb.lk/Incognito/GetLoadSheddingEvents", {
-            headers: {
-                requestverificationtoken: reqToken,
-                cookie: reqCookie,
-            },
-            cache: "no-cache",
-            body: `StartTime=${startTime}&EndTime=${endTime}`,
-            method: "POST",
-        });
-
-        const scheduleData = await scheduleRes.json();
-        const schedule = {};
-
-        scheduleData.forEach((item) => {
-            const gid = item.loadShedGroupId;
-            const stime = item.startTime.replace("T", " ");
-            const etime = item.endTime.replace("T", " ");
-
-            if (schedule[gid] === undefined) {
-                schedule[gid] = [{ startTime: stime, endTime: etime }];
-            } else {
-                schedule[gid].push({ startTime: stime, endTime: etime });
+                setSchedule(schedule);
+                setGeoAreas(geoAreas);
+            } catch (e) {
+                console.error(e);
+                setError(true);
             }
-        });
-
-        const geoAreas = JSON.parse(await fs.readFile(process.cwd() + "/data/geoAreas.json"));
-
-        return {
-            props: { schedule, geoAreas },
-        };
-    } catch (e) {
-        console.error(e);
-        return {
-            props: { schedule: null, geoAreas: null },
-        };
-    }
-};
-
-export default function Home({ schedule, geoAreas }) {
-    const [highlighted, setHighlighted] = useState("");
+        })();
+    }, []);
 
     return (
         <div>
@@ -77,11 +42,11 @@ export default function Home({ schedule, geoAreas }) {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <div className={styles.container}>
-                <Navbar geoAreas={geoAreas} setHighlighted={setHighlighted} />
-                <div className={styles.groups}>
-                    {schedule && geoAreas ? (
-                        Object.entries(schedule).map(([gid, timings], index) => {
+            {schedule && geoAreas ? (
+                <div className={styles.container}>
+                    <Navbar geoAreas={geoAreas} setHighlighted={setHighlighted} />
+                    <div className={styles.groups}>
+                        {Object.entries(schedule).map(([gid, timings], index) => {
                             const areas = geoAreas[gid];
 
                             return (
@@ -93,15 +58,29 @@ export default function Home({ schedule, geoAreas }) {
                                     highlighted={highlighted}
                                 />
                             );
-                        })
+                        })}
+                    </div>
+                </div>
+            ) : (
+                <div className={styles.suspense}>
+                    {error ? (
+                        <div className={styles["suspense-error"]}>
+                            <MdError size="3em" color="rgb(182, 58, 58)" />
+                            <h5>Something went wrong!</h5>
+                            <h5>Try again in a few minutes.</h5>
+                            <i>powercuts.lk</i>
+                        </div>
                     ) : (
-                        <div className={styles.error}>
-                            <h3>Something went wrong!</h3>
-                            <p>Please try again in few minutes!</p>
+                        <div className={styles["suspense-loading"]}>
+                            <CgSpinner
+                                className={styles["suspense-loading-spinner"]}
+                                size="2em"
+                                color="rgb(10, 88, 165)"
+                            />
                         </div>
                     )}
                 </div>
-            </div>
+            )}
         </div>
     );
 }
