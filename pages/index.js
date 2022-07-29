7df696e7fb51a1892be965bd814d4d9a9ff72975
Sakith B <pvsakith@gmail.com>
2022-07-30 01:08:15 +0530

    Fixing pt1

--- pages/index.js
+++ pages/index.js
@@ -19,54 +19,8 @@ export default function Home() {
     useEffect(() => {
         (async () => {
             try {
-                const pageRes = await fetch("https://cebcare.ceb.lk/Incognito/DemandMgmtSchedule");
-                const pageContent = await pageRes.text();
-
-                const domParser = new DOMParser();
-                const parsedDoc = domParser.parseFromString(pageContent, "text/html");
-
-                const reqToken = parsedDoc
-                    .getElementsByName("__RequestVerificationToken")[0]
-                    .getAttribute("value");
-
-                const reqCookie = pageRes.headers.get("set-cookie").split(";")[0];
-
-                const dateRef = new Date();
-                const startTime = `${dateRef.getFullYear()}-${
-                    dateRef.getMonth() + 1
-                }-${dateRef.getDate()}`;
-                dateRef.setDate(dateRef.getDate() + 1);
-                const endTime = `${dateRef.getFullYear()}-${
-                    dateRef.getMonth() + 1
-                }-${dateRef.getDate()}`;
-
-                const scheduleRes = await fetch(
-                    "https://cebcare.ceb.lk/Incognito/GetLoadSheddingEvents",
-                    {
-                        headers: {
-                            requestverificationtoken: reqToken,
-                            cookie: reqCookie,
-                        },
-                        cache: "no-cache",
-                        body: `StartTime=${startTime}&EndTime=${endTime}`,
-                        method: "POST",
-                    }
-                );
-
-                const scheduleData = await scheduleRes.json();
-                const schedule = {};
-
-                scheduleData.forEach((item) => {
-                    const gid = item.loadShedGroupId;
-                    const stime = item.startTime.replace("T", " ");
-                    const etime = item.endTime.replace("T", " ");
-
-                    if (schedule[gid] === undefined) {
-                        schedule[gid] = [{ startTime: stime, endTime: etime }];
-                    } else {
-                        schedule[gid].push({ startTime: stime, endTime: etime });
-                    }
-                });
+                const scheduleRes = await fetch("/api/getSchedule");
+                const schedule = await scheduleRes.json();
 
                 const geoRes = await fetch("/geoAreas.json");
                 const geoAreas = await geoRes.json();
